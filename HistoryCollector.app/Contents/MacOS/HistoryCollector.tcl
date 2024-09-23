c1439b5fb594e77d90e6b40af13c4f8238f38a8d
Blue-EMA <146757887+Blue-EMA@users.noreply.github.com>
2024-09-23 13:34:54 -0400

    .

--- HistoryCollector.app/Contents/MacOS/HistoryCollector.tcl
+++ HistoryCollector.app/Contents/MacOS/HistoryCollector.tcl
@@ -1,28 +1,60 @@
 #!/usr/bin/env wish
+
 package require Tk
 
-proc runScript{USB} {
-set script "ghistory.js"
-set command "node $script $USB"
-
-if { [catch {exec {*}$command} result]} {
-tk_messageBox -icon error -message "ghistory not working: $result"}
-
-else{tk_messageBox -icon info -message "Script worked: $result"}
+# Procedure to run the script
+proc runScript {USB} {
+    set script "/path/to/ghistory.js" ;# Use absolute path
+    if {![file exists $script]} {
+        tk_messageBox -icon error -message "Script not found at $script"
+        return
+    }
+    
+    set command "node $script $USB"
+    
+    # Use catch to handle errors during execution
+    if {[catch {exec $command} result]} {
+        # Get additional error info
+        set errorInfo [info errorstack]
+        tk_messageBox -icon error -message "ghistory not working: $result\nError Info: $errorInfo"
+    } else {
+        tk_messageBox -icon info -message "Script worked: $result"
+    }
 }
 
-wm title .msg "History Collector"
+# Set window title
+wm title . "History Collector"
 
-ttk::combobox .selectUSB -state readonly .selectUSB set "Select USB"
-set pathway [glob -nocomplain -directory /Volumes *] .selectUSB configure -values $pathway
+# Create a combobox for USB selection
+ttk::combobox .selectUSB -state readonly
 
-button .runButton -text "Enter" -command {set selectedUSB [.pathway get]
-	if {$selectedUSB ne "Select USB"}{
-		runScript $selectedUSB
-	}else{
-		tk_messageBox -icon warning -message "error select usb"
-	}
+# Get the list of mounted volumes
+set pathway [glob -nocomplain -directory /Volumes *]
+
+# Configure the combobox with the volumes
+.selectUSB configure -values $pathway
+
+# Create a button to run the script
+button .runButton -text "Enter" -command {
+    # Use catch to handle errors in this block
+    if {[catch {
+        set selectedUSB [.selectUSB get]
+        
+        # Debugging: Print selected USB for verification
+        puts "Selected USB: $selectedUSB"
+        
+        if {$selectedUSB ne ""} {
+            runScript $selectedUSB
+        } else {
+            tk_messageBox -icon warning -message "Please select a USB device."
+        }
+    } errMsg]} {
+        # Get additional error info
+        set errorInfo [info errorstack]
+        tk_messageBox -icon error -message "An error occurred: $errMsg\nError Info: $errorInfo"
+    }
 }
 
+# Pack the widgets
 pack .selectUSB
 pack .runButton
